681812e81187a8d53b0c348b3dd5b89f49a5d2c6
EranMani <eranmanim@gmail.com>
2026-02-13 12:12:44 +0200

    refacor: organize refund human feedback in a function. Add more print lines for better clarity

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ async def main():
     user2 = {"email": "user2@gmail.com", "query": "What are your business hours?"}
     user3 = {"email": "user1@gmail.com", "query": "I want to return order #999"}
 
+    requests = [user1, user2, user3]
+
     results = await asyncio.gather(
         run_triage(user1["query"], user1["email"]),
         run_triage(user2["query"], user2["email"]),
@@ -19,24 +21,28 @@ async def main():
 
     for i, result in enumerate(results):
         print(f"--- Test Case {i} ---")
-        print(f"Requires Human Approval: {result.requires_human_approval}")
+        print(f"--- System Analyzing Request: '{requests[i]["query"]}' ---")
 
-        print(f"Suggested Action: {result.suggested_action}")
-        print(f"Customer Reply: {result.customer_reply}")
+        #print(f"Suggested Action: {result.suggested_action}")
+        print(f"Dear client: : {result.customer_reply}")
 
         if result.requires_human_approval:
-            print("⚠️  STOP: High-stakes action detected.")
-            choice = input(f"Admin, do you approve the refund for Order #123? (Y/N): ").strip().upper()
-
-            if choice == "Y":
-                print("✅ Admin approved the refund for Order #123. Requesting execution...")
-
-                log = await process_refunds(ctx, order_id="#123")
-                print(f"Result: {log}")
-            else:
-                print("❌ Admin declined the refund for Order #123. Request cancelled.")
-
+            await refund_human_feedback(ctx)
+            
         print("--------------------------------")
+
+async def refund_human_feedback(ctx):
+    print("STOP: High-stakes action detected.")
+    # NOTE use hardcoded order number for now. Add it dynanically later
+    choice = input(f"Admin, do you approve the refund for Order #123? (Y/N): ").strip().upper()
+
+    if choice == "Y":
+        print("✅ Admin approved the refund for Order #123. Requesting execution...")
+
+        log = await process_refunds(ctx, order_id="#123")
+        print(f"Result: {log}")
+    else:
+        print("❌ Admin declined the refund for Order #123. Request cancelled.")
 
 
 if __name__ == "__main__":
