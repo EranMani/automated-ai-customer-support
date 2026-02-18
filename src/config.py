11948981a5ecb8f17f4439128a077f71735a583f
EranMani <eranmanim@gmail.com>
2026-02-18 08:59:22 +0200

    feat: add on_status callable to app context class. will be used to pass tools messages to the yield generator

--- src/config.py
+++ src/config.py
@@ -1,8 +1,15 @@
 """
     This module contains the configuration required for the application to run 
+    NOTE: a callable is anything you can call with parentheses (). functions are values. You can store a function in a variable, pass it to another function, put it in a list 
+"""
+
+"""
+Why do we use the on_status callable to pass string messages between two distant parts of the code
+Each tool call this on_status callable, and the generator in run_triage_stream_events picks up those messages and sends them to the client
 """
 
 from dataclasses import dataclass
+from typing import Awaitable, Callable
 from src.db import MockDB
 from dotenv import load_dotenv
 
@@ -13,6 +20,11 @@ class AppContext:
     """Application context that includes a connection to the mock database"""
     db: MockDB
     user_email: str
+    # an optional async function that accepts a status message string
+    # it takes one argument: a str
+    # Awaitable[None] - it returns something you can await (because it's async), and it doesn't return a meaningful value (None)
+    # on_status is an optional async function that takes a string message.
+    on_status: Callable[[str], Awaitable[None]] | None = None # Making it optional with a default of None
 
 CLASSIFIER_MODEL = "ollama:llama3.2"
 SPECIALIST_MODEL = "openai:gpt-4.1-mini"
